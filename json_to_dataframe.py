dabf0e1bbe7daf726ca0b524dfe8cfd9ab14da42
christopheQUENTIN <104220991+christopheQUENTIN@users.noreply.github.com>
2024-01-17 23:22:17 +0100

    Add files via upload

--- json_to_dataframe.py
+++ json_to_dataframe.py
@@ -1,3 +1,5 @@
+"""
+
 import json
 import pandas as pd
 
@@ -6,6 +8,46 @@ json_string = input("paste the json data >>> ")
 data_dict = json.loads(json_string)
 
 df = pd.DataFrame([data_dict])
+
+pd.set_option('display.max_columns', None)
+
+print(df)
+
+"""
+
+"""
+import json
+import pandas as pd
+
+json_string = input("paste the json data >>> ")
+
+data = json.loads(json_string)
+
+if isinstance(data, dict):
+   df = pd.DataFrame([data])
+elif isinstance(data, list):
+    df = pd.DataFrame(data)
+else:
+    raise("unable to convert json object into a python list or dictionnary , thus failing providing dataframe")
+
+#df = pd.DataFrame([datat])
+
+pd.set_option('display.max_columns', None)
+
+print(df)
+"""
+
+import json
+import pandas as pd
+
+json_string = input("paste the json data >>> ")
+
+data = json.loads(json_string)
+
+#df = pd.DataFrame([data])
+
+df = pd.DataFrame(data)
+
 
 pd.set_option('display.max_columns', None)
 
